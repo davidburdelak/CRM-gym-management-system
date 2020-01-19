d8c439d9fb169a057522b6eef7e8a1c9ecd866f7
David Burdelak <davidburdelak@gmail.com>
2020-01-19 16:40:38 +0100

    Code cleaning

--- gym_search.php
+++ gym_search.php
@@ -1,11 +1,8 @@
 <?php
+
 require('db.php');
 
-
 $name="";
-
-
-
 if (isset($_POST['name'])) {
 	echo "<div class='container'>";
 	echo "<table class='table table-bordered  table-hover mt-3'>";
@@ -19,33 +16,24 @@ if (isset($_POST['name'])) {
 	echo "</tr>";
 	echo "</div>";
 
-
 	$name=$_POST['name'];
-
-
-		$que=mysqli_query($conn,"SELECT * FROM `gym` WHERE CONCAT(`gym_id`,`gym_name`,`address`,`address`,`type`) LIKE '%".$name."%'");
+	
+	$que=mysqli_query($conn,"SELECT * FROM `gym` WHERE CONCAT(`gym_id`,`gym_name`,`address`,`address`,`type`) LIKE '%".$name."%'");
 		if(mysqli_num_rows($que) > 0){
-	
-	while($row=mysqli_fetch_array($que))
-	{
-		echo "<tr>";
-		echo "<td>".$row['gym_id']."</td>";
-		echo "<td>".$row['gym_name']."</td>";
-		echo "<td>".$row['address']."</td>";
-		echo "<td>".$row['type']."</td>";
-		echo "<td><a href='home.php?id=$row[gym_id]&info=update_gym'><i class='fas fa-pencil-alt'></i></a></td>";
-		echo  "<td><a href='home.php?id=$row[gym_id]&info=delete_gym'><i class='fas fa-trash-alt'></i></a></td>";
-		echo "</tr>";
-
-	}
-}else{
-	echo "<div class='alert alert-warning'><b>0 result</b></div>";
+			while($row=mysqli_fetch_array($que)){
+				echo "<tr>";
+				echo "<td>".$row['gym_id']."</td>";
+				echo "<td>".$row['gym_name']."</td>";
+				echo "<td>".$row['address']."</td>";
+				echo "<td>".$row['type']."</td>";
+				echo "<td><a href='home.php?id=$row[gym_id]&info=update_gym'><i class='fas fa-pencil-alt'></i></a></td>";
+				echo  "<td><a href='home.php?id=$row[gym_id]&info=delete_gym'><i class='fas fa-trash-alt'></i></a></td>";
+				echo "</tr>";
+			}
+		}
+		else {
+			echo "<div class='alert alert-warning'><b>0 result</b></div>";
+		}
 }
-	
-}
-
-
-
-
 	
 ?>
