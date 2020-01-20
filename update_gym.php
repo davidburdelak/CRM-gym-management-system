aa4227fdf3b65386f838a2030e57c892c77528a2
David Burdelak <davidburdelak@gmail.com>
2020-01-20 16:27:20 +0100

    Code cleaning

--- update_gym.php
+++ update_gym.php
@@ -1,41 +1,34 @@
 <?php
+
 require('db.php');
 
-
-
-
-
 if (isset($_REQUEST['gym'])) {
-  $gym_id = mysqli_real_escape_string($conn, $_REQUEST['id']);
-  $name = mysqli_real_escape_string($conn, $_REQUEST['name']);
-  $address = mysqli_real_escape_string($conn, $_REQUEST['address']);
-  $type = mysqli_real_escape_string($conn, $_REQUEST['type']);
-
-
-  $update_query="update gym set gym_id='$gym_id',gym_name='$name',address='$address',type='$type' where gym_id='".$_GET['id']."'";
-  $update_sql=mysqli_query($conn,$update_query);
-  $err="<div class='alert alert-success'><b>Gym Details updated</b></div>";
+	$gym_id = mysqli_real_escape_string($conn, $_REQUEST['id']);
+	$name = mysqli_real_escape_string($conn, $_REQUEST['name']);
+	$address = mysqli_real_escape_string($conn, $_REQUEST['address']);
+	$type = mysqli_real_escape_string($conn, $_REQUEST['type']);
+	
+	$update_query="update gym set gym_id='$gym_id',gym_name='$name',address='$address',type='$type' where gym_id='".$_GET['id']."'";
+	$update_sql=mysqli_query($conn,$update_query);
+	$err="<div class='alert alert-success'><b>Gym Details updated</b></div>";
 }
+
 $con=mysqli_query($conn,"select * from gym where gym_id='".$_GET['id']."'");
 
 $res=mysqli_fetch_assoc($con);  
 
-
-
 ?>
-
-
-
-
-
 
 <div class="container">
 	<form class="form-group mt-3" method="post" action="">
 		<div><h3>UPDATE GYM</h3></div>
-		 <?php  
-    echo @$err;
-
-    ?>
+		
+		<?php  
+		
+		echo @$err;
+		
+		?>
+		
 		<label class="mt-3">GYM ID</label>
 		<input type="text" name="id" value="<?php echo @$res['gym_id'];?>" class="form-control">
 		<label class="mt-3">GYM NAME</label>
