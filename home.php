7ca1fd1cc22e5aa0805ad0370c99d7b01245a039
David Burdelak <davidburdelak@gmail.com>
2020-01-20 12:48:53 +0100

    Code cleaning

--- home.php
+++ home.php
@@ -1,4 +1,5 @@
 <?php
+
 include("auth.php");
 
 ?>
@@ -6,11 +7,11 @@ include("auth.php");
 <!DOCTYPE html>
 <html>
 <head>
-
+	
 	<title>Management System - GYM</title>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="robots" content="noydir">
 	<meta name="robots" content="noodp">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
